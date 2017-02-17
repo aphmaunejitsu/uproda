9c9ce0143754364c91118aaa3a6f22c939e6ecda
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2017-02-17 21:53:34 +0700

    [変更] #28 イメージ ページデザイン
    やっぱセンスないわー

--- fuel/app/themes/skeleton/image/content/image.php
+++ fuel/app/themes/skeleton/image/content/image.php
@@ -4,13 +4,16 @@
 		<div class="nine columns">
 	  	<a href="<?php echo $src ?>" target="_blank"><img class='u-max-full-width image-detail' src="<?php echo $src; ?>"></a>
 		</div>
-		<div class="three columns" style="text-align:left">
+		<div class="three columns" >
 			<ul><?php if ( ! empty(\Arr::get($image, 'comment', null))): ?><li><?php echo \Arr::get($image, 'comment', null);?></li> <?php endif; ?>
 			<li><?php echo \Arr::get($image, 'original', null);?></li>
 			<li><?php echo \Arr::get($image, 'mimetype', null);?></li>
 			<li><?php echo \Num::format_bytes(\Arr::get($image, 'size', null), 1);?></li>
 			<li><?php echo \Arr::get($image, 'created_at', null);?></li>
-			<li><input type='text' class='u-full-width' value='<?php echo $src;?>'></li>
+			<li>
+				<input id='image-path' type='text' class='u-full-width' value='<?php echo $src;?>'>
+				<button class="btn button u-full-width" data-clipboard-target="#image-path">copy</button>
+			</li>
 			</ul>
 		</div>
 	</div>
