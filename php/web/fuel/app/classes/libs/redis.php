84a04622dea19b704be52fedfd26ff3a4ab3c83c
aphmau <aphmau_nejitsu@icloud.com>
2018-01-30 14:43:43 +0700

    [追加] Redisキャッシュ, トップページ5秒キャッシュ, 画像ページ1分キャッシュ

--- php/web/fuel/app/classes/libs/redis.php
+++ php/web/fuel/app/classes/libs/redis.php
@@ -1,2 +1,27 @@
 <?php
-class Libs_Redis extends \Redis_Db { }
+class Libs_Redis extends \Redis_Db
+{
+	public static function cached($name, $method, $params, $expire = 60, $force = false)
+	{
+		$redis = Libs_Redis::forge();
+		$key = $name.'_'.json_encode($method).'_'.json_encode($params).'_'.$expire;
+		if ( $force or (! ($result = $redis->get($key))))
+		{
+			if ( ! ($result = call_user_func_array($method, $params)))
+			{
+				//データがないことをキャッシュ
+				$redis->setex($key, $expire, 0);
+			}
+			else
+			{
+				$redis->setex($key, $expire, serialize($result));
+			}
+		}
+		else
+		{
+			$result = unserialize($result);
+		}
+
+		return $result;
+	}
+}
