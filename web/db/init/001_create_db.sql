1c03d3a280c15b8db04b416f2ee2c090fa75f0e2
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2018-08-06 22:08:44 +0900

    Mysql8対応

--- web/db/init/001_create_db.sql
+++ web/db/init/001_create_db.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `uproda` DEFAULT CHARACTER SET utf8mb4;
 USE `uproda`
 CREATE USER 'updater'@'%' IDENTIFIED BY '0p;/.Lo9';
-GRANT SELECT,UPDATE,INSERT,DELETE ON uproda.* TO 'updater'@'%';
+GRANT SELECT,UPDATE,INSERT,DELETE ON uproda.* TO 'updater'@'%'  WITH GRANT OPTION;;
 CREATE USER 'reader'@'%' IDENTIFIED BY '9Ol.,ki8';
-GRANT SELECT ON uproda.* TO 'reader'@'%';
+GRANT SELECT ON uproda.* TO 'reader'@'%'  WITH GRANT OPTION;;
 CREATE USER 'maintenance'@'%' IDENTIFIED BY '8ik,.lO9';
-GRANT ALL ON uproda.* TO 'maintenance'@'%';
+GRANT ALL ON uproda.* TO 'maintenance'@'%'  WITH GRANT OPTION;;
