7b6a9006f2a360a4b0ad5f9ba37640ec2576ffa3
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-02-15 20:42:35 +0900

    Add react

--- app/routes/web.php
+++ app/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
