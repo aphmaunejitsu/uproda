4e697958416a1b636144c7a6a66bb3667f3fb18b
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-02-21 11:28:03 +0900

    Add Header

--- app/resources/js/components/header/SiteLogo.js
+++ app/resources/js/components/header/SiteLogo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../../images/favicon.jpg';
 
 function SiteLogo() {
   return (
     <div className="SiteLogo">
-      ロゴ
+      <Link to="/">
+        <img src={Logo} alt="ネ実うpろだ 我々のコリブリ いやらしい・・・" />
+      </Link>
     </div>
   );
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+})->where('any', '.*');
