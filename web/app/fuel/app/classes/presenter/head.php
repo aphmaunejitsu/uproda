38feb7efbcd1aa81c0b78c540bebdc9171a238cb
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-02-27 20:51:03 +0900

    Add Image title

--- web/app/fuel/app/classes/presenter/head.php
+++ web/app/fuel/app/classes/presenter/head.php
@@ -4,7 +4,16 @@ class Presenter_Head extends Presenter_Uproda
 	public function view()
 	{
 		parent::view();
-		$this->title = Libs_Lang::get('common.title');
+
+        $title = Libs_Lang::get('common.title');
+
+        if (isset($this->param['image']))
+        {
+            $this->title = $title.' | '.$this->param['image']->basename;
+        } else {
+		    $this->title = $title;
+        }
+
 		$this->description = Libs_Lang::get('common.description');
 	}
 }
