b73496ae0cabfd0dbeecad5e1c46361cbf309ed5
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-03-16 23:18:15 +0900

    deal with dark mode

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ return [
             ]) : [],
         ],
 
-        'mysql-migrate' => [
+        'migrate' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
--- app/resources/js/components/HeaderBar.js
+++ app/resources/js/components/HeaderBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './header/Nav';
 import SiteLogo from './header/SiteLogo';
+import UpDialog from './header/UpDialog';
 
 function HeaderBar() {
   return (
     <header className="headerBar">
       <SiteLogo />
       <Nav />
+      <UpDialog />
     </header>
   );
 }
