f8530cbaaa1a6141ef8e56b9a6ebd7c3290e7fe4
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-03-18 08:41:06 +0900

    Add Top Page

--- app/resources/js/components/Top.js
+++ app/resources/js/components/Top.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Top() {
+  // const [error, setError] = React.useState(null);
+  // const [isLoaded, setIsLoaded] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+  const getImages = async () => {
+    const response = await axios.get('/api/v1/image/');
+    console.log(response.data);
+    setItems(response.data.data);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
-    <h1>Top</h1>
+    <div className="images">
+      {items.map(
+        (image) => (
+          <div key={image.id} className="thumbnail">
+            <Link to={image.detail}>
+              <img src={image.thumbnail} alt="" />
+            </Link>
+          </div>
+        ),
+      )}
+    </div>
   );
 }
 
