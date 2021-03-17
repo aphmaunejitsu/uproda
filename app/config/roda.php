4e325aa1945ae10ebe70c4becf45fd3a41c73d85
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-03-17 15:13:03 +0900

    Add build url trait

--- app/config/roda.php
+++ app/config/roda.php
@@ -4,5 +4,11 @@ return [
     'name'    => env('RODA_NAME', 'ネ実うpろだ'),
     'service' => [
         'cache' => env('RODA_SERVICE_CACHE', 60),
+    ],
+    'url'     => [
+        'image' => [
+            'base'      => env('RODA_URL_IMAGE_BASE', 'http://localhost'),
+            'thumbnail' => env('RODA_THUMBNAIL_DIR', 'thumbnail')
+        ]
     ]
 ];
