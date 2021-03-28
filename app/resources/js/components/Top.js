fe6a3620d3fa9e21889739af6e5337ca0b0f9b5a
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-03-28 19:25:12 +0900

    Add Infinity scroll

--- app/resources/js/components/Top.js
+++ app/resources/js/components/Top.js
@@ -1,44 +1,42 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
+
 import Thumbnail from './top/Thumbnail';
 import Loading from './common/Loading';
 
 function Top() {
-  const [error, setError] = React.useState(null);
-  const [isLoaded, setIsLoaded] = React.useState(false);
   const [items, setItems] = React.useState([]);
+  const [hasMore, setHasMore] = React.useState(true);
 
-  const getImages = async () => {
-    try {
-      const response = await axios.get('/api/v1/image/');
-      setItems(response.data);
-      setIsLoaded(true);
-    } catch (e) {
-      setError(true);
+  const getImages = async (p) => {
+    const response = await axios.get('/api/v1/image/', { params: { page: p } });
+    const data = response.data.data;
+
+    if (data.length < 1) {
+      setHasMore(false);
+      return;
     }
+
+    setItems([...items, ...data]);
   };
 
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  if (isLoaded) {
-    return (
-      <>
-        <div className="images">
-          {items.data.map((image) => (
-            <Thumbnail image={image} key={image.id} />
+  return (
+    <>
+      <div className="images">
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={getImages}
+          hasMore={hasMore}
+          loader={<Loading />}
+        >
+          {items.map((image) => (
+            <Thumbnail image={image} key={image.basename} />
           ))}
-        </div>
-      </>
-    );
-  }
-
-  if (error) {
-    return <div>error...</div>;
-  }
-
-  return <Loading />;
+        </InfiniteScroll>
+      </div>
+    </>
+  );
 }
 
 export default Top;
