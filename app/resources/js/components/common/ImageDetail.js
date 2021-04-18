0e8f2ffc3936e73066fbe6c94288714b81a277f4
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-18 21:51:52 +0900

    Image View become able to scroll

--- app/resources/js/components/common/ImageDetail.js
+++ app/resources/js/components/common/ImageDetail.js
@@ -1,26 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import PropTypes from 'prop-types';
 
 function ImageDetail({ image }) {
-  const [isLoadedImage, setIsLoadedImage] = useState(false);
-
   if (!image) {
     return null;
   }
 
-  useEffect(() => {
-    const img = new Image();
-    img.src = image.image;
-
-    img.onload = () => {
-      setIsLoadedImage(true);
-    };
-  }, []);
-
-  if (!isLoadedImage) {
-    return null;
+  let formatBytes = '0 Bytes';
+  if (image.size > 0) {
+    const k = 1024;
+    const bytes = Number(image.size);
+    const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    const num = parseFloat((bytes / (k ** i)).toFixed(2));
+    formatBytes = `${num}  ${units[i]}`;
   }
 
   return (
@@ -31,10 +26,23 @@ function ImageDetail({ image }) {
             alt={image.comment}
             effect="blur"
             src={image.image}
+            className="image"
           />
+          <div className="content">
+            <div className="title">
+              {image.comment ? image.comment : image.original}
+            </div>
+            <div className="size">
+              {formatBytes}
+            </div>
+          </div>
+          <div className="comments">
+            coment
+          </div>
         </div>
-        <div className="contents">
-          aaaaaaa
+        {/* .image-detail */}
+        <div className="footer">
+          footer
         </div>
       </div>
     </>
@@ -49,6 +57,8 @@ ImageDetail.propTypes = {
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
     comment: PropTypes.string.isRequired,
+    original: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
   }).isRequired,
 };
 
