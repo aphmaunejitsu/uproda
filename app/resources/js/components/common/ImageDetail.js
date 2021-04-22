35d2e65923b2a1d3def38a830d214c6625d3acb1
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-23 08:37:29 +0900

    sparate image view and detail

--- app/resources/js/components/common/ImageDetail.js
+++ app/resources/js/components/common/ImageDetail.js
@@ -1,45 +1,20 @@
 import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import PropTypes from 'prop-types';
+import ImageView from './ImageView';
 
 function ImageDetail({ image }) {
   if (!image) {
     return null;
   }
 
-  let formatBytes = '0 Bytes';
-  if (image.size > 0) {
-    const k = 1024;
-    const bytes = Number(image.size);
-    const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'ZB', 'YB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    const num = parseFloat((bytes / (k ** i)).toFixed(2));
-    formatBytes = `${num}  ${units[i]}`;
-  }
-
   return (
     <>
       <div className="image-detail">
-        <div className="view">
-          <LazyLoadImage
-            alt={image.comment}
-            effect="blur"
-            src={image.image}
-            className="image"
-          />
-          <div className="content">
-            <div className="title">
-              {image.comment ? image.comment : image.original}
-            </div>
-            <div className="size">
-              {formatBytes}
-            </div>
-            <div className="comments">
-              coment
-            </div>
-          </div>
-        </div>
+        <LazyLoadComponent effect="blur">
+          <ImageView image={image} />
+        </LazyLoadComponent>
         <div className="footer">
           footer
         </div>
@@ -50,14 +25,7 @@ function ImageDetail({ image }) {
 
 ImageDetail.propTypes = {
   image: PropTypes.shape({
-    basename: PropTypes.string.isRequired,
-    detail: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    comment: PropTypes.string.isRequired,
-    original: PropTypes.string.isRequired,
-    size: PropTypes.number.isRequired,
   }).isRequired,
 };
 
