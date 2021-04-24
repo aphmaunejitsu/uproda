3d39d8cebbac8777b6ae5e05fc69fe0cc2b6fd61
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-24 15:16:18 +0900

    Add Detail page

--- app/resources/js/components/Detail.js
+++ app/resources/js/components/Detail.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import PropTypes from 'prop-types';
+import ImageDetail from './detail/Main';
 
 function Detail() {
   const { hash } = useParams();
   return (
-    <h1>
-      Image: &nbsp;
-      {hash}
-    </h1>
+    <>
+      <LazyLoadComponent id={image.basename}>
+        <ImageDetail image={image} />
+      </LazyLoadComponent>
+    </>
   );
 }
+
+Detail.propTypes = {
+  image: PropTypes.shape({
+    basename: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Detail;
