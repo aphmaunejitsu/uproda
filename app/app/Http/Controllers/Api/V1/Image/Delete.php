a490a2070f2fc50c9a615bc13dc88c616924679f
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-25 13:38:24 +0900

    Add delete image controller

--- app/app/Http/Controllers/Api/V1/Image/Delete.php
+++ app/app/Http/Controllers/Api/V1/Image/Delete.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1\Image;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\v1\image\DeleteRequest;
 use App\Services\ImageService;
 use Illuminate\Http\Request;
 
@@ -20,8 +21,20 @@ class Delete extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(DeleteRequest $request)
     {
+        $param = $request->validated();
+        extract($param);
 
+        if (($deletedImage = $this->service->deleteImage($basename, $delkey))) {
+            return response()->json(
+                [
+                    'message' => __('response.delete.failed'),
+                ],
+                404
+            );
+        }
+
+        return response()->json(['message' => __('response.delete.success'), 204);
     }
 }
