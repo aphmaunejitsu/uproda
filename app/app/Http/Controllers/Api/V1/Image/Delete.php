4fba1359a1d14590494fd5af81836c7b4805960b
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-25 09:42:11 +0900

    remove unmount flag on image detail

--- app/app/Http/Controllers/Api/V1/Image/Delete.php
+++ app/app/Http/Controllers/Api/V1/Image/Delete.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers\Api\V1\Image;
 
 use App\Http\Controllers\Controller;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 
 class Delete extends Controller
 {
+    public $service;
+    public function __construct(ImageService $image)
+    {
+        $this->service = $image;
+    }
+
     /**
      * Handle the incoming request.
      *
@@ -15,6 +22,6 @@ class Delete extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+
     }
 }
