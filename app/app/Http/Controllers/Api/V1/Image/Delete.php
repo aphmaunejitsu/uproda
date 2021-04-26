10532422a78474f02525b6a67b6e3a2ae94d9d73
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-04-26 18:00:09 +0900

    Add delete image

--- app/app/Http/Controllers/Api/V1/Image/Delete.php
+++ app/app/Http/Controllers/Api/V1/Image/Delete.php
@@ -26,7 +26,6 @@ class Delete extends Controller
     public function __invoke(DeleteRequest $request)
     {
         $param = $request->validated();
-        Log::info(__METHOD__, $param);
         extract($param);
 
         if (!($deletedImage = $this->service->deleteImage($basename, $delkey))) {
