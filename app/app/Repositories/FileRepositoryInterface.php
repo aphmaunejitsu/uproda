35456ec72245aacafc483752094e39719f04a96f
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-01 18:55:08 +0900

    Add FileRepository::getGeometryByFile

--- app/app/Repositories/FileRepositoryInterface.php
+++ app/app/Repositories/FileRepositoryInterface.php
@@ -3,9 +3,11 @@
 namespace App\Repositories;
 
 use App\Models\Image;
+use Illuminate\Http\UploadedFile;
 
 interface FileRepositoryInterface
 {
     public function getGeometry(string $basename, ?string $ext);
+    public function getGeometryByFile(UploadedFile $file);
     public function deleteByImage(Image $image): bool;
 }
