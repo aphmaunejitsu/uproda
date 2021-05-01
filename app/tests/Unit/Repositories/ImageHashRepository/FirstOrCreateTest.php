6c7a459875e47698bcc8d262146ab8cf6b5150aa
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-01 10:29:54 +0900

    modify ImageHashRepository test

--- app/tests/Unit/Repositories/ImageHashRepository/FirstOrCreateTest.php
+++ app/tests/Unit/Repositories/ImageHashRepository/FirstOrCreateTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Repositories\ImageHashRepository;
 use App\Models\ImageHash;
 use App\Repositories\ImageHashRepository;
 use App\Repositories\ImageHashRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -38,16 +39,21 @@ class FirstOrCreateTest extends TestCase
      */
     public function testFirstOrCreate()
     {
+        Carbon::setTestNow(new Carbon('2021-05-01 00:01:02'));
+
         $imageHash = $this->repo->firstOrCreate('abc', false, 'test');
 
         $this->assertInstanceOf(ImageHash::class, $imageHash);
         $this->assertEquals('abc', $imageHash->hash);
         $this->assertFalse($imageHash->ng);
         $this->assertEquals('test', $imageHash->comment);
+        $this->assertEquals('2021-05-01 00:01:02', $imageHash->created_at);
+        $this->assertEquals('2021-05-01 00:01:02', $imageHash->updated_at);
     }
 
     public function testAlredyExists()
     {
+        Carbon::setTestNow(new Carbon('2020-05-01 00:01:02'));
         ImageHash::factory()->create([
             'hash'    => 'xyz',
             'ng'      => true,
@@ -59,5 +65,7 @@ class FirstOrCreateTest extends TestCase
         $this->assertEquals('xyz', $imageHash->hash);
         $this->assertTrue($imageHash->ng);
         $this->assertEquals('already', $imageHash->comment);
+        $this->assertEquals('2020-05-01 00:01:02', $imageHash->created_at);
+        $this->assertEquals('2020-05-01 00:01:02', $imageHash->updated_at);
     }
 }
