0b914778dbdbef3091bdb8cd08af40bf51ac18e0
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-02 17:43:59 +0900

    modify generate thumbnail gif test

--- app/tests/Unit/Repositories/FileRepository/GenerateThumbnailGifTest.php
+++ app/tests/Unit/Repositories/FileRepository/GenerateThumbnailGifTest.php
@@ -7,6 +7,7 @@ use App\Repositories\FileRepositoryInterface;
 use App\Repositories\FileRepository;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 /**
  * @group upload
@@ -38,6 +39,10 @@ class GenerateThumbnailGifTest extends TestCase
         $file = UploadedFile::fake()->image('abc.gif', 500, 500);
 
         $result = $this->repo->generateThumbnailGif($file, 'xyz', 'gif');
+
+        $image = Image::make(Storage::disk('image')->readStream('/x/thumbnail/xyz.gif'));
         Storage::disk('image')->assertExists('/x/thumbnail/xyz.gif');
+        $this->assertEquals(config('roda.thumbnail.height'), $image->height());
+        $this->assertEquals(config('roda.thumbnail.width'), $image->width());
     }
 }
