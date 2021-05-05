61c8add256191c24d9b1a2504b8404898f6f877f
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-05 17:51:33 +0900

    Add Upload Single test

--- app/tests/Feature/Api/V1/Image/UploadTest.php
+++ app/tests/Feature/Api/V1/Image/UploadTest.php
@@ -2,15 +2,20 @@
 
 namespace Tests\Feature\Api\V1\Image;
 
+use App\Libs\Traits\BuildImagePath;
+use App\Models\Image;
 use App\Models\ImageHash;
 use App\Services\Traits\ImageTrait;
+use App\Services\UploadService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
+use Mockery\MockInterface;
 
 /**
- * @group Upload
+ * @group upload
  * @group Controller
  * @group UploadTest
  */
@@ -19,6 +24,7 @@ class UploadTest extends TestCase
     use RefreshDatabase;
     use WithFaker;
     use ImageTrait;
+    use BuildImagePath;
 
     private $url;
 
@@ -135,5 +141,56 @@ class UploadTest extends TestCase
                          'file' => ['It is NG file']
                      ]
                  ]);
+    }
+
+    public function testSingleUpload()
+    {
+        Storage::fake('image');
+        Storage::fake('tmp');
+
+        $file = UploadedFile::fake()->image('test.jpg');
+        $file->size(1);
+        $hash = $this->faker->uuid;
+        $json = compact('hash', 'file');
+        ImageHash::factory()->create([
+            'hash' => $this->getHash($file),
+            'ng'   => false
+        ]);
+        $response = $this->postJson($this->url, $json);
+
+        $result = $response->getContent();
+        $res = json_decode($result);
+
+        $image = $this->buildImagePath($res->data->basename, $res->data->ext);
+        $thumbnail = $this->buildThumbnailPath($res->data->basename, $res->data->t_ext);
+        Storage::disk('image')->assertExists($image);
+        Storage::disk('image')->assertExists($thumbnail);
+
+        $response->assertStatus(201);
+    }
+
+    public function testFailedSingleUpload()
+    {
+        $this->mock(
+            UploadService::class,
+            function (MockInterface $m) {
+                $m->shouldReceive('uploadSingleFile')->andReturn(
+                    null
+                );
+            }
+        );
+
+        $file = UploadedFile::fake()->image('test.jpg');
+        $file->size(1);
+        $hash = $this->faker->uuid;
+        $json = compact('hash', 'file');
+        ImageHash::factory()->create([
+            'hash' => $this->getHash($file),
+            'ng'   => false
+        ]);
+        $response = $this->postJson($this->url, $json);
+
+        $response->assertStatus(400)
+            ->assertJson(['message' => 'アップロードできませんでした']);
     }
 }
