7f4fa0a3b4ae0221693786db88405c1610783391
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-05 21:37:58 +0900

    Add ChunkFileRepository

---
+++ app/app/Repositories/ChunkFileRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace APp\Repositories;
+
+use App\Repositories\ChunkFileRepositoryInterface;
+use Illuminate\Support\Facades\Redis;
+
+class ChunkFileRepository implements ChunkFileRepositoryInterface
+{
+    public function addChunk(string $uuid, int $start, string $file)
+    {
+        return Redis::zadd($uuid, $start, $file);
+    }
+
+    public function getChunks(string $uuid)
+    {
+        return Redis::zrange($uuid, 0, -1);
+    }
+
+    public function remove(string $uuid)
+    {
+        return Redis::del($uuid);
+    }
+}
