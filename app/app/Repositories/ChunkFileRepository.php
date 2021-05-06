49b3fe0865db3544f609c6740b9166097a3d2105
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-06 19:07:22 +0900

    Add ChunkFiles teable

--- app/app/Repositories/ChunkFileRepository.php
+++ app/app/Repositories/ChunkFileRepository.php
@@ -2,11 +2,18 @@
 
 namespace App\Repositories;
 
+use App\Models\ChunkFile;
 use App\Repositories\ChunkFileRepositoryInterface;
 use Illuminate\Support\Facades\Redis;
 
 class ChunkFileRepository implements ChunkFileRepositoryInterface
 {
+    private $model;
+
+    public function __construct(ChunkFile $model)
+    {
+        $this->model = $model;
+    }
     public function addChunk(string $uuid, int $start, string $file)
     {
         return Redis::zadd($uuid, $start, $file);
@@ -20,5 +27,19 @@ class ChunkFileRepository implements ChunkFileRepositoryInterface
     public function remove(string $uuid)
     {
         return Redis::del($uuid);
+    }
+
+    public function createByUuid(string $uuid)
+    {
+        return $this->model->create([
+            'uuid' => $uuid
+        ]);
+    }
+
+    public function getByUuid(string $uuid)
+    {
+        return $this->model
+                    ->where('uuid', $uuid)
+                    ->first();
     }
 }
--- app/app/Repositories/ChunkFileRepositoryInterface.php
+++ app/app/Repositories/ChunkFileRepositoryInterface.php
@@ -7,4 +7,7 @@ interface ChunkFileRepositoryInterface
     public function addChunk(string $uuid, int $start, string $file);
     public function getChunks(string $uuid);
     public function remove(string $uuid);
+
+    public function createByUuid(string $uuid);
+    public function getByUuid(string $uuid);
 }
