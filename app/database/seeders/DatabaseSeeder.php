6c15b8908d5514b152a70f89b74708b2b70c5194
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-05-09 14:36:48 +0900

    Add deny_word seeder

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\DenyWordSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            DenyWordSeeder::class
+        ]);
     }
 }
