7373ff5f94cb1f25bb281389552f4ed57685250e
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-08-15 15:59:59 +0900

    fixed not working verify google recaptcha

--- app/app/Services/GoogleRecaptchaServices/Verify.php
+++ app/app/Services/GoogleRecaptchaServices/Verify.php
@@ -7,6 +7,8 @@ use App\Services\GoogleRecaptchaService;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
+use function PHPUnit\Framework\isNull;
+
 class Verify extends GoogleRecaptchaService
 {
     public function __construct(
@@ -15,15 +17,23 @@ class Verify extends GoogleRecaptchaService
         $this->repo = $repo;
     }
 
-    public function __invoke(string $uuid, string $ipaddr, string $token, int $expire = 300)
+    public function __invoke(?string $uuid, ?string $ipaddr, string $token, int $expire = 300)
     {
+        if (is_null($uuid)) {
+            return false;
+        }
+
+        if (is_null($ipaddr)) {
+            return false;
+        }
+
         if (($result = Cache::get($uuid))) {
             return $result['success'];
         }
 
         $response = $this->repo->verify($token, $ipaddr);
 
-        Cache::put($uuid, $response->json(), $expire));
+        Cache::put($uuid, $response->json(), $expire);
         return $response->json()['success'];
     }
 }
