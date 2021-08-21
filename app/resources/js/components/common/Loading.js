43618e8f20d959662e16173be900082491fe07c9
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-08-21 19:01:39 +0900

    Add dmm-ad

--- app/resources/js/components/common/Loading.js
+++ app/resources/js/components/common/Loading.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '1rem',
+    width: '100%',
+    'margin-top': 'auto',
+    'margin-button': 'auto',
+    display: 'flex',
+    'justify-content': 'center',
+  },
+});
 
 function Loading() {
+  const styles = useStyles();
+
   return (
-    <div className="loading">
+    <div className={styles.root}>
       <CircularProgress />
     </div>
   );
