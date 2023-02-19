e73ef0657d36658334c46e4c6e1e4d993cec4c5d
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2023-02-19 10:03:54 +0900

    ロゴの配置を変更

--- app/resources/js/components/header/SiteLogo.js
+++ app/resources/js/components/header/SiteLogo.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Logo from '../../../images/favicon.jpg';
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    'align-items': 'center',
+  },
+  img: {
+    height: 36,
+    width: 36,
+  },
+  span: {
+    'margin-left': '0.5rem',
+    color: 'black',
+  },
+});
+
 function SiteLogo() {
+  const classes = useStyles();
   return (
-    <div className="SiteLogo">
-      <Link to="/">
-        <img src={Logo} alt="ネ実うpろだ 我々のコリブリ いやらしい・・・" />
+    <div className={classes.root}>
+      <Link
+        to="/"
+        style={{ textDecoration: 'none' }}
+      >
+        <img
+          className={classes.img}
+          src={Logo}
+          alt={process.env.MIX_RODA_IMG_DESCRIPTION}
+        />
+        <span className={classes.span}>{process.env.MIX_RODA_SUBTITLE}</span>
       </Link>
     </div>
   );
