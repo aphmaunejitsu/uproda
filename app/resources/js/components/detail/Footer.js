6b8d416334af3b800efeaef30a45ef4bc3abf49f
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2024-05-19 10:24:20 +0900

    ダウンローボタンの追加

--- app/resources/js/components/detail/Footer.js
+++ app/resources/js/components/detail/Footer.js
@@ -4,6 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import { Link } from 'react-router-dom';
 import CopyToClipBoard from 'react-copy-to-clipboard';
 import ToolTip from '@material-ui/core/Tooltip';
@@ -57,6 +58,11 @@ function Main({ image }) {
         </ToolTip>
         <ShareTwitterButton image={image} />
         <ShareButton image={image} />
+        <a href={`${image.image}`} download>
+          <IconButton>
+            <CloudDownloadIcon />
+          </IconButton>
+        </a>
         <Link
           to={`${image.detail}`}
           rel="noopener noreferrer"
