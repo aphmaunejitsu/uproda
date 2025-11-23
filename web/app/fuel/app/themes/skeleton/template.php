ff3cdea02caf12088c44a73c570ab84f1f89f25f
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2025-11-23 19:26:29 +0900

    Fix URL parameters in Amazon ad links for consistency in template.php

--- web/app/fuel/app/themes/skeleton/template.php
+++ web/app/fuel/app/themes/skeleton/template.php
@@ -9,7 +9,7 @@
 	</header>
     <div id='top-ad' class="amazon" style="display:flex; justify-content:center; padding:10px;">
 		<div id='top-ad-amazon' style="width:85%; max-width:750px;">
-			<a href="https://www.amazon.co.jp/hz/audible/mlp/membership/premiumplus/monthly/paid/?_encoding=UTF8&actionCode=AMAP30DTRIAL3191028256260&ref_=cct_cg_audible_1a1&pf_rd_p=e26c997d-cfd4-4e66-8714-209dbdb54cd3&pf_rd_r=EFJA3NNEHPGESPF6NCWD?rafale-22" target="_blank">
+			<a href="https://www.amazon.co.jp/hz/audible/mlp/membership/premiumplus/monthly/paid/?_encoding=UTF8&actionCode=AMAP30DTRIAL3191028256260&ref_=cct_cg_audible_1a1&pf_rd_p=e26c997d-cfd4-4e66-8714-209dbdb54cd3&pf_rd_r=EFJA3NNEHPGESPF6NCWD&rafale-22" target="_blank">
 				<img src="https://m.media-amazon.com/images/G/09/2025/x-site/BFW/9mh1pa7/event_page/LU/BFW25_EP_003_LU_PageHeader_mb_750x200_JP.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
@@ -21,7 +21,7 @@
 	</div>
     <div id='top-ad' class="amazon" style="display:flex; justify-content:center; padding:10px;">
 		<div id='top-ad-amazon' style="width:85%; max-width:750px;">
-			<a href="https://www.amazon.co.jp/hz/audible/mlp/membership/premiumplus/monthly/paid/?_encoding=UTF8&actionCode=AMAP30DTRIAL3191028256260&ref_=cct_cg_audible_1a1&pf_rd_p=e26c997d-cfd4-4e66-8714-209dbdb54cd3&pf_rd_r=EFJA3NNEHPGESPF6NCWD?rafale-22" target="_blank">
+			<a href="https://www.amazon.co.jp/hz/audible/mlp/membership/premiumplus/monthly/paid/?_encoding=UTF8&actionCode=AMAP30DTRIAL3191028256260&ref_=cct_cg_audible_1a1&pf_rd_p=e26c997d-cfd4-4e66-8714-209dbdb54cd3&pf_rd_r=EFJA3NNEHPGESPF6NCWD&rafale-22" target="_blank">
 				<img src="https://m.media-amazon.com/images/G/09/2025/x-site/BFW/9mh1pa7/event_page/LU/BFW25_EP_003_LU_PageHeader_mb_750x200_JP.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
