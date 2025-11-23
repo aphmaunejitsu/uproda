6a68b475f2f8ccd924c23eaed30f3237c20c10cb
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2025-11-23 19:49:00 +0900

    Update Amazon ad link and image in template.php for improved promotion

--- web/app/fuel/app/themes/skeleton/template.php
+++ web/app/fuel/app/themes/skeleton/template.php
@@ -21,8 +21,8 @@
 	</div>
     <div id='top-ad' class="amazon" style="display:flex; justify-content:center; padding:10px;">
 		<div id='top-ad-amazon' style="width:85%; max-width:750px;">
-			<a href="https://www.amazon.co.jp/hz/audible/mlp/membership/premiumplus/monthly/paid/?_encoding=UTF8&actionCode=AMAP30DTRIAL3191028256260&ref_=cct_cg_audible_1a1&pf_rd_p=e26c997d-cfd4-4e66-8714-209dbdb54cd3&pf_rd_r=EFJA3NNEHPGESPF6NCWD&tag=rafale-22" target="_blank">
-				<img src="https://m.media-amazon.com/images/G/09/2025/x-site/BFW/9mh1pa7/event_page/LU/BFW25_EP_003_LU_PageHeader_mb_750x200_JP.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
+			<a href="https://www.amazon.co.jp/b/ref=adbl_JP_as_0068?ie=UTF8&node=7471076051&tag=rafale-22" target="_blank">
+				<img src="https://m.media-amazon.com/images/G/09/AudibleJP/BlackFriday2025/AmazonHomestead/1031-04_AmazonHomestead_2025_BF_3M99JPY_Storefront_Hero_DT_1280x500_text_V2.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
     </div>
