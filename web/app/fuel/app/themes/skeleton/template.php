5e404680700ae37b875c867b85d05d70f774b766
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2025-12-03 13:23:40 +0900

    Update Amazon ad links and images in template.php for improved promotion

--- web/app/fuel/app/themes/skeleton/template.php
+++ web/app/fuel/app/themes/skeleton/template.php
@@ -9,13 +9,13 @@
 	</header>
     <div id='top-ad' class="amazon" style="display:flex; align-items:center; flex-direction:column; padding:10px;">
 		<div id='top-ad-amazon-1' style="width:85%; max-width:750px;margin-bottom:10px;">
-			<a href="https://amzn.to/4oVgCYS" target="_blank">
-				<img src="https://m.media-amazon.com/images/G/09/2025/x-site/BFW/9mh1pa7/event_page/LU/BFW25_EP_003_LU_PageHeader_mb_750x200_JP.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
+			<a href="https://amzn.to/4pbOkty" target="_blank">
+				<img src="https://m.media-amazon.com/images/G/09/associates/General/t-furusato_02.jpg" alt="ふるさと納税" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
 		<div id='top-ad-amazon-2' style="width:85%; max-width:750px;">
-			<a href="https://www.amazon.co.jp/b/ref=adbl_JP_as_0068?ie=UTF8&node=7471076051&tag=rafale-22" target="_blank">
-				<img src="https://m.media-amazon.com/images/G/09/AudibleJP/BlackFriday2025/AmazonHomestead/1031-04_AmazonHomestead_2025_BF_3M99JPY_Storefront_Hero_DT_1280x500_text_V2.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
+			<a href="https://amzn.to/4406LZC" target="_blank">
+				<img src="https://m.media-amazon.com/images/G/09/2025/kindle/NTA/Octc/associate-special-fto_DT_3000x600_20250904.jpg" alt="Kindle本" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
     </div>
@@ -26,8 +26,8 @@
 	</div>
     <div id='bottom-ad' class="amazon" style="display:flex; justify-content:center; padding:10px;">
 		<div id='bottom-ad-amazon' style="width:85%; max-width:750px;">
-			<a href="https://www.amazon.co.jp/b/ref=adbl_JP_as_0068?ie=UTF8&node=7471076051&tag=rafale-22" target="_blank">
-				<img src="https://m.media-amazon.com/images/G/09/AudibleJP/BlackFriday2025/AmazonHomestead/1031-04_AmazonHomestead_2025_BF_3M99JPY_Storefront_Hero_DT_1280x500_text_V2.jpg" alt="Amazonオーディブル無料体験" style="width:100%; height:auto; border:none;">
+			<a href="https://amzn.to/4406LZC" target="_blank">
+				<img src="https://m.media-amazon.com/images/G/09/2025/kindle/NTA/Octc/associate-special-fto_DT_3000x600_20250904.jpg" alt="Kindle本" style="width:100%; height:auto; border:none;">
 			</a>
 		</div>
     </div>
